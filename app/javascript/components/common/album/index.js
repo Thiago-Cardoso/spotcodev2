7094338f6795f71fa9565603f71e0d569b778b3b
Thiago-Cardoso <thiagocardosooti@gmail.com>
2020-04-21 22:16:28 -0300

    Development dinamic of data for api

--- app/javascript/components/common/album/index.js
+++ app/javascript/components/common/album/index.js
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import { Heading, Image } from 'react-bulma-components';
 import styled from 'styled-components'
+import { Link } from "react-router-dom";
  
 const DivVSpaced = styled.div`
   margin-top: 10px;
@@ -8,13 +9,13 @@ const DivVSpaced = styled.div`
 
 const Album = (props) => {
   return(
-    <Fragment>
+    <Link to={`/album/${props.id}`}>
       <Image src={props.cover_url}/>
       <DivVSpaced>
         <Heading size={6} className='has-text-white'>{props.title}</Heading>
         <Heading size={6} className='has-text-white' subtitle>{props.artist_name}</Heading>
       </DivVSpaced>
-    </Fragment>
+    </Link>
   );
 }
 export default Album;
--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment} from 'react';
 import NavbarFooter from '../../components/common/navbar_footer ';
+import Albums from '../../components/albums';
 
 const AlbumScreen = () => {
     return(
         <Fragment>
+            <Albums />
            <NavbarFooter/>
         </Fragment>
     )
