656302bcb2197baa9b0a73d798a2385b7f68d65c
Thiago-Cardoso <thiagocardosooti@gmail.com>
2020-04-22 01:06:24 -0300

    Development of favorities

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,11 +1,16 @@
 import React, { Fragment} from 'react';
 import NavbarFooter from '../../components/common/navbar_footer ';
+import { Heading } from 'react-bulma-components';
+import Favorites from '../../components/favorites';
 
 const FavoritesScreen = () => {
     return(
         <Fragment>
-            <NavbarFooter/>
-        </Fragment>
+
+          <Heading className='has-text-centered has-text-white'>Favoritos</Heading>
+          <Favorites/>
+        <NavbarFooter/>
+      </Fragment>
     )
 }
 
